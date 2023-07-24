ce3fb7dfcfaff259327302af1187b34f098c8501
rustam <user@isroilovO.fido.uz>
2023-07-24 15:24:19 +0500

    full

---
+++ script.js
@@ -0,0 +1,35 @@
+var red = document.querySelector('.div1')
+var yellow = document.querySelector('.div2')
+var  blue= document.querySelector('.div3')
+var black = document.querySelector('.div4')
+var x = document.querySelector('.div5')
+var body = document.querySelector('.body')
+red.addEventListener('click', () => {
+    body.style.background = 'red'
+}
+
+
+)
+blue.addEventListener('click', () => {
+    body.style.background = 'blue'
+}
+
+
+)
+yellow.addEventListener('click', () => {
+    body.style.background = 'yellow'
+}
+
+
+)
+black.addEventListener('click', () => {
+    body.style.background = 'black'
+}
+)
+x.addEventListener('click', () => {
+    body.style.background = 'white'
+}
+
+
+
+)
